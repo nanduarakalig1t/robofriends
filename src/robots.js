a52962234a0bceeb7447d686efa2e6bf3358bb93
nanduarakalig1t <67098933+nanduarakalig1t@users.noreply.github.com>
2020-11-25 11:39:08 +0530

    final project files for robofriends

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'first robot',
+	username: 'first',
+	email: 'first@mail'
+},
+{
+	id: 2,
+	name: 'second robot',
+	username: 'second',
+	email: 'second@mail'
+},
+{
+	id: 3,
+	name: 'third robot',
+	username: 'third',
+	email: 'third@mail'
+},
+{
+	id: 4,
+	name: 'fourth robot',
+	username: 'fourth',
+	email: 'fourth@mail'
+},
+{
+	id: 5,
+	name: 'fifth robot',
+	username: 'fifth',
+	email: 'fifth@mail'
+},
+{
+	id: 6,
+	name: 'sixth robot',
+	username: 'sixth',
+	email: 'sixth@mail'
+},
+{
+	id: 7,
+	name: 'seventh robot',
+	username: 'seventh',
+	email: 'seventh@mail'
+},
+{
+	id: 8,
+	name: 'eight robot',
+	username: 'eight',
+	email: 'eight@mail'
+},
+{
+	id: 9,
+	name: 'ninth robot',
+	username: 'ninth',
+	email: 'ninth@mail'
+},
+{
+	id: 10,
+	name: 'tenth robot',
+	username: 'tenth',
+	email: 'tenth@mail'
+}
+]
